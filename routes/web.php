6f5e306b21148a0090e48c1b19e6f7e3fb5f2812
laupommares <laurapommares@gmail.com>
2025-08-29 17:53:53 -0300

    moving dashboard /

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ThemeController;
 use App\Livewire\Dashboard;
 
-Route::get('/', [ThemeController::class, 'app']);
-Route::get('/dashboard', Dashboard::class);
+/* Route::get('/', [ThemeController::class, 'app']); */
+Route::get('/', Dashboard::class);
